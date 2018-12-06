19145e614b15b03ea95a07c743c7979c8a3b27d0
Michael Sawlani <michael.sawlani@facebook.com>
2018-12-06 10:15:40 -0500

    Initial Commit

---
+++ Memorandum Shopper/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Memorandum Shopper
+//
+//  Created by Michael Sawlani on 12/6/18.
+//  Copyright © 2018 Michael Sawlani. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Memorandum Shopper/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Memorandum Shopper
+//
+//  Created by Michael Sawlani on 12/6/18.
+//  Copyright © 2018 Michael Sawlani. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
