b003562323a6862dc32ebc060893efb99f55e4ef
Michael Sawlani <michael.sawlani@facebook.com>
2019-01-08 12:02:46 -0500

    now saves list to firebase

--- Memorandum Shopper/ListViewController.swift
+++ Memorandum Shopper/ListViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 var grocerylist = [""]
 var asileNum = [""]
@@ -14,7 +15,14 @@ var asileNum = [""]
 class ListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var List: UITableView!
-
+    var refItem:DatabaseReference!
+    @IBAction func Save(_ sender: Any) {
+        ItemList()
+    }
+    
+    @IBAction func Load(_ sender: Any) {
+        
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return (grocerylist.count)
@@ -44,8 +52,33 @@ class ListViewController: UIViewController, UITableViewDataSource, UITableViewDe
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        refItem = Database.database().reference().child("items")
+        
+        refItem.observe(DataEventType.value, with: {(snapshot) in
+            if snapshot.childrenCount>0{
+                grocerylist.removeAll()
+                
+                for items in snapshot.children.allObjects as![DataSnapshot]{
+                    let itemObject = items.value as? [String: AnyObject]
+                    let itemName = itemObject?["Item Name"]
+                    let id = itemObject?["id"]
+                    
+                    
+                    
+                    grocerylist.append(itemName as! String)
+                }
+            }
+        })
     }
-
+    func ItemList(){
+    let key = refItem.childByAutoId().key
+        let item = ["id":key!,
+                    "Item Name": grocerylist] as [String : Any]
+        
+        refItem.child(key!).setValue(item)
+        
+    }
 
 }
 
