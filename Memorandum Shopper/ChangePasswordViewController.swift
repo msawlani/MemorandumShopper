bd54487011ae17cb3143ab7abf6802d2fe247e14
Michael <sawlani.michael@yahoo.com>
2019-01-24 12:44:12 -0500

    Now you can change password with firebase account

--- Memorandum Shopper/ChangePasswordViewController.swift
+++ Memorandum Shopper/ChangePasswordViewController.swift
@@ -8,10 +8,50 @@
 
 import Foundation
 import UIKit
+import FirebaseAuth
 
 class ChangePasswordVC: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var Password: UITextField!
     
+    @IBOutlet weak var reenterPassword: UITextField!
     
+    @IBAction func ChangPassword(_ sender: Any) {
+        guard let password = Password.text else{return}
+        guard let reEnterPassword = reenterPassword.text else{return}
+        Auth.auth().currentUser?.updatePassword(to: password){ error in
+            if let error = error{
+                print(error)
+                let alert = UIAlertController(title: "Failed to Change Password", message: "Must enter Password", preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                
+                self.present(alert, animated: true)
+            }else{
+                if password == reEnterPassword{
+                let message = "You can now re-login with your new password"
+                    let alert = UIAlertController(title: "Successful Change to Password", message: message, preferredStyle: .alert)
+                    
+                    alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: {(action) in
+                        do{
+                            try Auth.auth().signOut()
+                        }catch let Logouterror{
+                            print(Logouterror)
+                        }
+                        
+                        self.performSegue(withIdentifier: "Login", sender: self)
+                        
+                    }))
+                
+                self.present(alert, animated: true)
+                }
+                else{
+                    let alert = UIAlertController(title: "Failed to Change Password", message: "Both passwords must match", preferredStyle: .alert)
+                    
+                    alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                }
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
     }
