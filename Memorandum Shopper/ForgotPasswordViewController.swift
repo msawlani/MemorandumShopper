276d631ff71f31a3be6f52d70f547f8397b06471
Michael <sawlani.michael@yahoo.com>
2019-01-24 10:54:28 -0500

    added forgot password and sends you a link with the email you entered

---
+++ Memorandum Shopper/ForgotPasswordViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ForgotPasswordViewController.swift
+//  Memorandum Shopper
+//
+//  Created by Michael Sawlani on 1/24/19.
+//  Copyright © 2019 Michael Sawlani. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import FirebaseAuth
+
+class ForgotPasswordVC: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var Email: UITextField!
+    @IBAction func ResetPasswordLink(_ sender: Any) {
+        guard let email = Email.text else { return }
+        Auth.auth().sendPasswordReset(withEmail: email) {error in
+            if let error = error{
+                print(error)
+                let alert = UIAlertController(title: "Failed to Send Link", message: "Must enter email", preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                
+                self.present(alert, animated: true)
+            }else{
+                let message = "Check Email: " + email
+                let alert = UIAlertController(title: "Successful Sending Link", message: message, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: {(action) in                self.performSegue(withIdentifier: "Login", sender: self)
+
+                }))
+                
+                self.present(alert, animated: true)
+                
+            }
+            
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+}
